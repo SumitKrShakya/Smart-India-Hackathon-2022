2864af7740349b2a354694211a5bb309eaa11c73
Bhanu Arora <mrbhanuarora@gmail.com>
2022-03-28 00:13:29 +0530

    model integrated

---
+++ backend/controllers/plate_detection.js
@@ -0,0 +1,27 @@
+const { spawn } = require("child_process");
+const videoModel = require("../models/video");
+const { imgRecog } = require("../utils/ocrText");
+const { verify } = require("./vehicle");
+const path = require("path");
+const detectPlate = async (req,res,next) => {
+    let {recordingId} = req.body;
+    let  videoData = await videoModel.findById(recordingId);
+    // run model
+    const pyProg = spawn('python', [path.join(__dirname,"..","pyModel.py"),videoData.recording_url]);
+    pyProg.stdout.on("data", async function(data){
+        // ocr of image
+        let plateNumber = await imgRecog(data);
+        // match with db
+        req.registrationNo = plateNumber;
+        verify(req,res,next);
+    });
+    pyProg.stderr.on("data",async (data) => {
+        console.log(data.toString());
+    })
+}
+detectPlate({
+    body : {
+        recordingId : "6240ae6f0ea7245671a0f762"
+    }
+})
+module.exports = detectPlate;
---
+++ backend/utils/ocrText.js
@@ -0,0 +1,64 @@
+// const { createWorker } = require('tesseract.js');
+
+// const worker = createWorker({
+//   logger: m => console.log(m)
+// });
+
+
+// const imgRecog = async (req,res) => {
+  //   await worker.load();
+  //   await worker.loadLanguage('eng');
+  //   await worker.initialize('eng');
+  //   const { data: { text } } = await worker.recognize('./public/ab.jpeg');
+  //   console.log(text);
+//   await worker.terminate();
+//   return res.redirect('./public/try.jpeg');
+// }
+// module.exports = {
+  //     imgRecog
+  // }
+  
+  const path = require("path");
+  const aws = require("aws-sdk");
+  aws.config.update({
+    secretAccessKey : "",
+    accessKeyId: "",
+    region : "us-east-2"
+  });
+  const fs = require("fs");
+  const textRact = new aws.Textract({region : "us-east-2"});
+  
+  exports.imgRecog = (imageName) => {
+    return new Promise((resolve,reject) => {
+      fs.readFile(path.join(__dirname, imageName),{},(err,data) => {
+        if(err){
+          console.log(err);
+          return err;
+        }
+        console.log(data);
+        textRact.analyzeDocument({
+          Document : {
+            Bytes : data
+          },
+          FeatureTypes : ["FORMS"]
+        },(err,data1) => {
+          if(err){
+            console.log(err);
+            return err;
+          } 
+          console.log(data1);
+          let data2 = data1.Blocks;
+          let str = "";
+          for(let i = 0;i<data2.length ;i++){
+            if(data2[i].Text){
+              str += (data2[i].Text + " ") ;
+            }else{
+              console.log(data2[i].Geometry);
+            }
+          }
+          console.log(str);
+          resolve(str);
+        })
+      } );
+    })
+  }
